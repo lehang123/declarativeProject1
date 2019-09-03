74dc92e50a876d0b7fbfa3b8aafc2974c77f2b9c
Gong Lehan <>
2019-09-04 02:11:39 +1000

    jjj

--- Test6.hs
+++ Test6.hs
@@ -1,30 +1,46 @@
 module Test6 where
 
+
 fun :: [Bool] -> Bool
-fun [w,x,y,z] = d
-    where ny = not y
-          nz = not z
-          nq = not q
-          u1 = ny || z
-          q2 = ny || x
-          u2 = nz || y
-          q1 = not x || y
-          u = (u1 && u2)
-          q = (q1 && q2)
-          e = a && nz
-          g = (b || d) && not (u && x || v)
-          a = g || (y && z)
-          f = x || u1
-          b = q2 &&  (nq || nz || w)
-          v = (w && y)
-          c = not (v || x) || u
-          d = w || ( nq || not u1)
+fun [w,x,y,z] = c
+    where e = not ((z || xoy) && (w || yz))
+          a = e || (z && (xoyow))
+          f = x || yz
+          g = a && nots
+          d = xoyow && nots
+          c = (e || f) && ( not ((x && yoz) && nots))
+          b = e && f || (x && ny) || (w && z)
+          xoyow =  xoy || w
+          yz = ny || z
+          ny = not y
+          xoy = x || y
+          nots = not ( x && y && z)
+          yoz = y || z
 
---           c = not (v||(x && not u))
---          b = q2 && not (q && z && not w)
---          d = w || not ( q && u1)
+--     (not x && not y && not z) || (not w && y && not z)
+--     c = (e || f) && ( not ((x && yoz) && nots))   c = (not ((y || z)) && b)
 
-    -- 29 gates used
+-- fun :: [Bool] -> Bool
+-- fun [w,x,y,z] = g
+--     where ny = not y
+--           nz = not z
+--           u1 = ny || z
+--           q2 = ny || x
+--           u2 = nz || y
+--           u = (u1 && u2)
+--           nq = ((x && ny) || not q2)
+--           e = a && nz
+--           g = (b || d) && not (u && x || v)
+--           a = g || (y && z)
+--           f = x || u1
+--           b = q2 &&  (nq || nz || w)
+--           v = (w && y)
+--           c = not (x || v) || u
+--           d = w || ( nq || not u1)
+
+    -- 28 gates used
+
+--      g = (b || d) && not (u && x || v)
 
 im :: Bool -> Bool -> Bool
 im a b = not a || b
